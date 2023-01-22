f782600fa15093cded5bb85dde4f83bf75e2ebf3
nonsens <nonsens1212@gmail.com>
2023-01-22 11:25:29 +0400

    Добавил Entity Framework

---
+++ JeopardyGame.Persistance/ContextDB.cs
@@ -0,0 +1,14 @@
+﻿using JeopardyGame.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JeopardyGame.Persistance
+{
+    public class ContextDB: DbContext
+    {
+        public DbSet<Pack> Packs { get; set; }
+        public DbSet<Round> Rounds { get; set; }
+        public DbSet<Theme> Themes { get; set; }
+        public DbSet<Question> Questions { get; set; }      
+    
+    }
+}
