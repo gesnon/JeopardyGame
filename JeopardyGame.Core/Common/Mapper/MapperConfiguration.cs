3fa08bd14cdbc44c787d5af550de13db57e6e6ae
Vladislav Chinnov <v.chinnov@sreda.capital>
2023-01-22 12:08:52 +0400

    [~] Фикс конфликтов

--- JeopardyGame.Core/Common/Mapper/MapperConfiguration.cs
+++ JeopardyGame.Core/Common/Mapper/MapperConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
-using JeopardyGame.Domain.DTO;
+using JeopardyGame.Core.Models;
 using JeopardyGame.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JeopardyGame.Core.Common.Mapper
 {
@@ -13,24 +8,24 @@ namespace JeopardyGame.Core.Common.Mapper
     {
         public MapperConfiguration()
         {
-            this.CreateMap<Question, QuestionDTO>()
+            this.CreateMap<Question, QuestionDto>()
             .ForMember(_ => _.Text, opt => opt.MapFrom(i => i.Text))
             .ForMember(_ => _.Id, opt => opt.MapFrom(i => i.Id))
             .ForMember(_ => _.Price, opt => opt.MapFrom(i => i.Price))
             .ForMember(_ => _.QuestionType, opt => opt.MapFrom(i => i.QuestionType))
             .ForMember(_ => _.DataContent, opt => opt.MapFrom(i => i.DataContent));
 
-            this.CreateMap<Theme, ThemeDTO>()
+            this.CreateMap<Theme, ThemeDto>()
             .ForMember(_ => _.Title, opt => opt.MapFrom(i => i.Title))
             .ForMember(_ => _.Id, opt => opt.MapFrom(i => i.Id))
             .ForMember(_ => _.Questions, opt => opt.MapFrom(i => i.Questions));
 
-            this.CreateMap<Round, RoundDTO>()
+            this.CreateMap<Round, RoundDto>()
             .ForMember(_ => _.Order, opt => opt.MapFrom(i => i.Order))
             .ForMember(_ => _.Id, opt => opt.MapFrom(i => i.Id))
             .ForMember(_ => _.Themes, opt => opt.MapFrom(i => i.Themes));
 
-            this.CreateMap<Pack, PackDTO>()
+            this.CreateMap<Pack, PackDto>()
             .ForMember(_ => _.Title, opt => opt.MapFrom(i => i.Title))
             .ForMember(_ => _.Id, opt => opt.MapFrom(i => i.Id))
             .ForMember(_ => _.Author, opt => opt.MapFrom(i => i.Author))
